7d1565b1d97f768d5f3f84da244938d15f1df9c1
Diana T <135925668+PhoenixFirePop@users.noreply.github.com>
2023-08-09 19:18:52 -0500

    added jump

--- player.js
+++ player.js
@@ -4,26 +4,47 @@ export class Player {
         this.width = 100;
         this.height = 90;
         this.x = 0;
-        this.y = 400;
-        // this.image = player; same as below
-        this.image = document.getElementById('player')
+        this.y = this.game.height - this.height;
+        this.vy = 0 //vertical speed/velocity
+        this.weight = 1 //will make the character have a sense of gravity (fall when in the air)
+        this.image = document.getElementById('player')  // this.image = player; same
         this.speed = 0;
         this.maxSpeed = 10;
     }
     update(input){
-        this.x += this.speed;
-        if (input.includes('ArrowRight')) this.x++;
-        else if (input.includes('ArrowLeft')) this.x--;
-        else if (input.includes('ArrowUp')) this.y--;
-        else if (input.includes('ArrowDown')) this.y++;
-        else this.speed = 0;
 
-        // this.x++; if just move right
+        // //basic all directional movement - a bit too slow
+        // if (input.includes('ArrowRight')) this.x++;
+        // else if (input.includes('ArrowLeft')) this.x--;
+        // else if (input.includes('ArrowUp')) this.y--;
+        // else if (input.includes('ArrowDown')) this.y++;
+        // else this.speed = 0;
+
+        //side movement
+        this.x += this.speed;
+        if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
+        else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
+        else this.speed = 0;
+        //to make sure the char stays within game area
+        if(this.x < 0) this.x = 0;
+        if(this.x > this.gamewidth - this.width) this.x = this.game.width - this.width;
+        //jump
+        else if (input.includes('ArrowUp') && this.onGround()) this.vy -= 10;
+        this.y += this.vy;
+        if(!this.onGround()) this.vy += this.weight;
+        else this.vy = 0;
+        // else if (input.includes('ArrowDown')) this.y++;
+
+
 
     }
     draw(context){
-        context.fillStyle = '';
-        context.fillRect(this.x, this.y, this.width, this.height);
+        // context.fillStyle = '';
+        // context.fillRect(this.x, this.y, this.width, this.height);
         context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+
+    onGround(){
+        return this.y >= this.game.height - this.height;
     }
 }
